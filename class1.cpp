0c0595be8c08804e05a9b09e55e77afd98713cb6
Rawan Yousry <rawanysoliman@gmail.com>
2025-01-05 13:08:16 +0200

    added all files

---
+++ class1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+class helloJava
+{
+public:
+    void PrintHelloWorld()
+    {
+        cout<<"hello world"<<endl;
+    }
+};
+
+
+
+int main()
+{
+helloJava  hw;
+hw.PrintHelloWorld();
+    return 0;
+}
---
+++ class2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class helloITI
+{
+public:
+    void PrintHelloITI()
+    {
+        cout<<"hello ITI"<<endl;
+    }
+};
+
+
+
+int main()
+{
+helloITI h;
+h.PrintHelloITI();
+    return 0;
+}
